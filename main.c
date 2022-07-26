ff02b0a1bed0f3385ab44b331741a0e27e05e0bf
HirohitoHigashi <higashi@s-itoc.jp>
2022-07-26 11:51:42 +0900

    refactoring main.c

--- main.c
+++ main.c
@@ -29,13 +29,11 @@
 #include "mrubyc.h"
 #include "mrbc_firm.h"
 
-
 #include "pic32mx.c"
 
 
 #define MEMORY_SIZE (1024*40)
 uint8_t memory_pool[MEMORY_SIZE];
-uint8_t t_count = 0;
 
 
 /*
@@ -64,107 +62,120 @@ void _mon_putc( char c )
 }
 
 
-static void c_pin_init(mrb_vm *vm, mrb_value *v, int argc) {
-    pin_init();
-}
+/*
+  Timer functions.
 
-void timer_init() {
-    TMR1 = TMR2 = TMR3 = 0x0;
-    PR1 = PR2 = 10000;
-    PR3 = 10;
-    T1CON = T2CON = T3CON = 0x8000;
-}
-
-int check_timeout(void)
+  using Timer1
+        16bit modes.
+	1kHz (1ms/cycle)
+  see   DS60001105G
+        14.3.4.2 16-BIT SYNCHRONOUS COUNTER INITIALIZATION STEPS
+*/
+static void tick_timer_init( void )
 {
-    int i;
-    for( i = 0; i < 50; i++ ) {
-        LATAbits.LATA0 = 1;
-        __delay_ms( 30 );
-        LATAbits.LATA0 = 0;
-        __delay_ms( 30 );
-        if( uart_can_read_line( &uart1_handle ) ) return 1;
-    }
-    return 0;
+  T1CON = 0;		// count PBCLK 1:1
+  TMR1 = 0;
+  PR1 = PBCLK / 1000;
+
+  IFS0CLR= 1 << _IFS0_T1IF_POSITION;	// Clear interrupt.
+  IPC1bits.T1IP = 1;	// Interrupt priority.
+  IPC1bits.T1IS = 0;
+  IEC0bits.T1IE = 1;	// Enable interrupt
+  T1CONbits.ON = 1;
 }
 
-/* mruby/c writer */
-
-void __ISR(_TIMER_1_VECTOR, IPL1AUTO) _T1Interrupt (  ){
-    mrbc_tick();
-    IFS0CLR= 1 << _IFS0_T1IF_POSITION;
-}
-
-void __ISR(_TIMER_2_VECTOR, IPL2AUTO) _T2Interrupt (  ){
-    t_count++;
-    IFS0CLR = 1 << _IFS0_T2IF_POSITION;
+// Timer1 interrupt handler.
+void __ISR(_TIMER_1_VECTOR, IPL1AUTO) timer1_isr( void )
+{
+  mrbc_tick();
+  IFS0CLR = (1 << _IFS0_T1IF_POSITION);
 }
 
 
+/*!
+  Choose to enter programming mode or run mode.
+*/
+static int check_timeout( void )
+{
+  int i;
+  for( i = 0; i < 50; i++ ) {
+    LATAbits.LATA0 = 1;
+    __delay_ms( 30 );
+    LATAbits.LATA0 = 0;
+    __delay_ms( 30 );
+    if( uart_can_read_line( &uart1_handle ) ) return 1;
+  }
+  return 0;
+}
 
+
+/*!
+  initialize pin assign.
+*/
+static void c_pin_init(mrb_vm *vm, mrb_value *v, int argc)
+{
+  pin_init();
+}
+
+
+/*!
+  main
+*/
 int main(void)
 {
-    /* module init */
-    pin_init();
-    i2c_init();
-    adc_init();
-    uart_init();
-    timer_init();
+  /* module init */
+  pin_init();
+  interrupt_init();
+  uart_init();
+  tick_timer_init();
 
-    /*Enable the interrupt*/
-#if 0
-    IPC9bits.U2IP = 4;
-    IPC9bits.U2IS = 3;
-    IFS1bits.U2RXIF = 0;
-    IFS1bits.U2TXIF = 0;
-    IEC1bits.U2RXIE = 1;
-    IEC1bits.U2TXIE = 0;
-#endif
-    IEC0bits.T1IE = 1;
-    IEC0bits.T2IE = 1;
-    IPC6bits.FCEIP = 1;
-    IPC6bits.FCEIS = 0;
-    IPC1bits.T1IP = 1;
-    IPC1bits.T1IS = 0;
-    IPC2bits.T2IP = 2;
-    IPC2bits.T2IS = 0;
-    INTCONbits.MVEC = 1;
-    __builtin_enable_interrupts();
+  __delay_ms(1000);
+  static const char ST_MSG[] = "\r\n\x1b(B\x1b)B\x1b[0m\x1b[2JRboard v*.*, mruby/c v3.1 start.\n";
+  hal_write( 0, ST_MSG, sizeof(ST_MSG) );
 
-    mrbc_print("\r\n\x1b(B\x1b)B\x1b[0m\x1b[2JRboard v*.*, mruby/c v3.1 start.\n");
 
-    if (check_timeout()){
-        /* IDE code */
-        add_code();
-	memset( memory_pool, 0, sizeof(memory_pool));
-    };
+  if (check_timeout()){
+    add_code();
+    //    memset( memory_pool, 0, sizeof(memory_pool));	// TODO
+  };
 
-    /* mruby/c */
-    mrbc_init(memory_pool, MEMORY_SIZE);
-    mrbc_define_method(0, mrbc_class_object, "pinInit", c_pin_init);
-    mrbc_init_class_adc(0);
-    mrbc_init_class_i2c(0);
-    mrbc_init_class_uart(0);
-    mrbc_init_class_digital(0);
-    mrbc_init_class_pwm(0);
-    mrbc_init_class_onboard(0);
-    mrbc_init_class_spi(0);
 
-    const uint8_t *fl_addr = (uint8_t*)FLASH_SAVE_ADDR;
-    static const char RITE[4] = "RITE";
-    while( strncmp( fl_addr, RITE, sizeof(RITE)) == 0 ) {
-      mrbc_create_task(fl_addr, 0);
+  /* init mruby/c VM */
+  mrbc_init(memory_pool, MEMORY_SIZE);
+  mrbc_define_method(0, mrbc_class_object, "pinInit", c_pin_init);
+  mrbc_init_class_adc(0);
+  mrbc_init_class_i2c(0);
+  mrbc_init_class_uart(0);
+  mrbc_init_class_digital(0);
+  mrbc_init_class_pwm(0);
+  mrbc_init_class_onboard(0);
+  mrbc_init_class_spi(0);
 
-      // get a next irep.
-      uint32_t size = 0;
-      int i;
-      for( i = 0; i < 4; i++ ) {
-	size = (size << 8) | fl_addr[8 + i];
-      }
-      fl_addr += ALIGN_ROW_SIZE( size );
+  /* load the ruby programs on flash memory. */
+  const uint8_t *fl_addr = (uint8_t*)FLASH_SAVE_ADDR;
+  static const char RITE[4] = "RITE";
+  while( strncmp( fl_addr, RITE, sizeof(RITE)) == 0 ) {
+    mrbc_create_task(fl_addr, 0);
+
+    // get a next irep.
+    uint32_t size = 0;
+    int i;
+    for( i = 0; i < 4; i++ ) {
+      size = (size << 8) | fl_addr[8 + i];
     }
+    fl_addr += ALIGN_ROW_SIZE( size );
+  }
 
-    T1CONbits.ON = 1;
-    mrbc_run();
-    return 1;
+  /* and run! */
+  mrbc_run();
+
+#if 0
+  while(1) {
+    LATAbits.LATA0 = 1;
+    __delay_ms( 1000 );
+    LATAbits.LATA0 = 0;
+    __delay_ms( 1000 );
+  }
+#endif
+  return 1;
 }
